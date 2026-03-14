73a118c3a5643ae7d21b20942ee60972de9a922c
CynthiaStephan <s.cynthia972@gmail.com>
2026-03-14 10:11:04 +0100

    project init

---
+++ server/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.config import settings
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "message": "test",
+        "environment": settings.environment
+    }
+    
